3d52375ba77119449f980251c95bcac26f5f7e72
antonisalimjob <antoni.salim.job@gmail.com>
2026-09-25 06:55:15 +0700

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
   const requestUrl = new URL(req.url);
   const origin = requestUrl.origin;
